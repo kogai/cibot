088932a2015f400a3c9a136863e8c4a787cc6369
小飼慎一 <kogaishinichi@kogaishinichi-no-MacBook-Pro.local>
2016-01-16 15:58:42 +0900

    [change] ディレクトリ構成を変更

--- robot.js
+++ robot.js
@@ -1,67 +1,10 @@
 import Botkit from 'botkit';
-import CircleCI from 'circleci';
 
-import { createSelectString } from 'utils';
-import { startHttpd } from 'httpd.js';
-
-const ci = new CircleCI({ auth: process.env.CIBOT_CIRCLECI_TOKEN });
-
-function rebuild(bot, msg) {
-  bot.reply(msg, ':hammer_and_wrench: OK, your projects fetching...');
-
-  ci.getProjects()
-  .then((projects)=> {
-    return new Promise((resolve, reject)=> {
-      bot.startConversation(msg, (err, conv)=> {
-        if (err) {
-          return reject({ conv, err });
-        }
-        resolve({ conv, projects });
-      });
-    });
-  })
-  .then(({ conv, projects })=> {
-    return new Promise((resolve)=> {
-      conv.ask(`Choose....\n${createSelectString(projects)}`, (res, nextConv)=> {
-        const selected = projects[Number(res.text) - 1];
-        const selectedName = `${selected.username}/${selected.reponame}`;
-
-        nextConv.say(`:robot_face: Your choice: \`${selectedName}\``);
-        nextConv.next();
-        resolve({ selected });
-      });
-    });
-  })
-  .then(({ selected })=> {
-    return new Promise((resolve)=> {
-      ci.getBuilds({
-        username: selected.username,
-        project: selected.reponame,
-        limit: 1,
-      })
-      .then((builds)=> {
-        resolve({ build: builds[0] });
-      });
-    });
-  })
-  .then(({ build })=> {
-    ci.retryBuild({
-      username: build.username,
-      project: build.reponame,
-      build_num: build.build_num,
-    })
-    .then((rebuilded)=> {
-      const buildDetail = `${rebuilded.branch} #${rebuilded.build_num} ${rebuilded.subject}`;
-      bot.reply(msg, `:hammer_and_wrench: Start rebuilding \`${buildDetail}\` ...`);
-    });
-  })
-  .catch((err)=> {
-    bot.reply(msg, `:robot_face: Rebuild is failed... check heroku logs`);
-    console.log(err);
-  });
-}
+import { rebuild } from 'controllers/rebuild.js';
+import { startHttpd } from 'lib/utils.js';
 
 export function run() {
+  // to avoid heroku timeout
   startHttpd();
 
   // Initialize
--- utils.js
+++
@@ -1,5 +0,0 @@
-export function createSelectString(projects) {
-  return projects.map((select, index)=> {
-    return `[${index + 1}] \`${select.username}/${select.reponame}\``;
-  }).join('\n');
-}
